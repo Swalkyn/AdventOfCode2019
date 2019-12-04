77baa5e9c81738ef109618c22d748b5c13d3df19
Sylvain Kuchen <sylvaink@bluewin.ch>
2019-12-04 04:27:46 +0100

    complete day 3 part 2

--- src/main/scala/day3/Main.scala
+++ src/main/scala/day3/Main.scala
@@ -20,7 +20,9 @@ object Main extends App {
     case object H extends Orientation { val constCoord = _._2; val diffCoord = _._1 }
     case object V extends Orientation { val constCoord = _._1; val diffCoord = _._2 }
     
-    case class Line(val s: Coord, val e: Coord, val o: Orientation) {
+    case class Line(val s: Coord, val e: Coord, val o: Orientation, p: Int) {
+        val path = p + distance(s, e)
+
         def intersect = (other: Line) =>
              ((((this.s._1 <= other.s._1 && other.s._1 <= this.e._1) || (this.e._1 <= other.s._1 && other.s._1 <= this.s._1)) 
             || ((this.s._1 <= other.e._1 && other.e._1 <= this.e._1) || (this.e._1 <= other.e._1 && other.e._1 <= this.s._1))) 
@@ -31,7 +33,7 @@ object Main extends App {
             &&(((other.s._1 <= this.s._1 && this.s._1 <= other.e._1) || (other.e._1 <= this.s._1 && this.s._1 <= other.s._1)) 
             || ((other.s._1 <= this.e._1 && this.e._1 <= other.e._1) || (other.e._1 <= this.e._1 && this.e._1 <= other.s._1))))
 
-        def closestIntersection(other: Line): Option[Coord] =
+        def intersectionWith(other: Line): Option[Coord] =
             if (this.intersect(other)) {
                 if (this.o == other.o) {
                     val f = this.o.diffCoord
@@ -39,16 +41,32 @@ object Main extends App {
                         Integer.max(Integer.min(f(this.s), f(this.e)), Integer.min(f(other.s), f(other.e))),
                         Integer.min(Integer.max(f(this.s), f(this.e)), Integer.max(f(other.s), f(other.e)))
                     )
-                    Some((this.o.constCoord(this.s), closestToZero(lower, upper)))
+                    
+                    this.o match {
+                        case H => Some((closestToZero(lower, upper), this.o.constCoord(this.s)))
+                        case V => Some((this.o.constCoord(this.s), closestToZero(lower, upper)))
+                    }
                 }
-                else Some((this.o.constCoord(this.s), other.o.constCoord(other.s)))
+                else this.o match {
+                    case H => Some((other.o.constCoord(other.s), this.o.constCoord(this.s)))
+                    case V => Some((this.o.constCoord(this.s), other.o.constCoord(other.s)))
+                }
             }
             else None
+        
+        def pathToIntersectionWith(other: Line): Option[Int] = {
+            this.intersectionWith(other).flatMap(
+                i => Some(this.path + other.path - distance(this.e, i) - distance(other.e, i))
+            )
+        }
         
         def closestToZero(lower: Int, upper: Int): Int =
             if (lower <= 0 && upper >= 0) 0
             else if (lower > 0) lower
             else upper
+        
+        def distance(c1: Coord, c2: Coord): Int =
+            Math.abs(c1._1 - c2._1) + Math.abs(c1._2 - c2._2)
     }
     
     val testInput = List("R8,U5,L5,D3", "U7,R6,D4,L4")
@@ -70,15 +88,19 @@ object Main extends App {
             case Left | Right => H
         }
 
-    def lineFromSegment(c: Coord, s: Segment): Line =
-        Line(c, (s._1.hops(c._1, s._2), s._1.vops(c._2, s._2)), orientationFromDirection(s._1))
+    def lineFromSegment(c: Coord, s: Segment, p: Int): Line =
+        Line(c, (s._1.hops(c._1, s._2), s._1.vops(c._2, s._2)), orientationFromDirection(s._1), p)
 
-    val wireAsLines = wires.map(_.foldLeft(List(Line((0, 0), (0, 0), H)))
-        ((acc, s) => lineFromSegment(acc.head.e, s) :: acc).reverse.drop(1))
+    val wireAsLines = wires.map(_.foldLeft(List(Line((0, 0), (0, 0), H, 0)))
+        ((acc, s) => lineFromSegment(acc.head.e, s, acc.head.path) :: acc).reverse.drop(1))
 
-    val intersections = wireAsLines(0).flatMap(l1 => wireAsLines(1).flatMap(l2 => l1.closestIntersection(l2)))
+    val intersections = wireAsLines(0).flatMap(l1 => wireAsLines(1).flatMap(l2 => l1.intersectionWith(l2)))
 
-    val withDistances = intersections.map(c => (c, Math.abs(c._1) + Math.abs(c._2))).sortBy(_._2).drop(1)
+    val distances = intersections.map(c => Math.abs(c._1) + Math.abs(c._2)).sorted.drop(1)
+
+    val paths = wireAsLines(0).flatMap(l1 => wireAsLines(1).flatMap(l2 => l1.pathToIntersectionWith(l2))).sorted.drop(1)
     
-	println(withDistances.take(10))
+    println(distances.head)
+    println(paths.head)
+    
 }
